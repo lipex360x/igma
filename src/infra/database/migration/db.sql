b9ddc2c06c0c0db006f31d14e8027401e7ed2dcf
Felipe Bambirra <lipex360@gmail.com>
2024-01-21 13:18:40 +0000

    feat: account input updated

--- src/infra/database/migration/db.sql
+++ src/infra/database/migration/db.sql
@@ -6,5 +6,6 @@ CREATE TABLE igma.account (
   account_id uuid PRIMARY KEY,
 	name text NOT NULL,
 	cpf text NOT NULL,
+	birth_date TIMESTAMP NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
