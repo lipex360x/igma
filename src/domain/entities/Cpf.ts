fb260d2198ed591d186e23dbc5a21ac106d4d03b
Felipe Bambirra <lipex360@gmail.com>
2024-01-21 14:24:14 +0000

    feat: VO cpf updated

--- src/domain/entities/Cpf.ts
+++ src/domain/entities/Cpf.ts
@@ -1,3 +1,54 @@
 export class Cpf {
-  constructor(readonly value: string) {}
+  private cpf: string
+
+  constructor(value: string) {
+    if (!this.validate(value)) throw new Error('Invalid CPF')
+    this.cpf = this.removeMask(value)
+  }
+
+  public getCpf() {
+    return this.cpf
+  }
+
+  private validate(value: string) {
+    if (!value) return false
+    const cpf = this.removeMask(value)
+    if (!this.isValidLength(cpf)) return false
+    if (this.allEqualDigits(cpf)) return false
+    return this.mountCheckDigit(cpf) === this.extractCheckDigit(cpf)
+  }
+
+  private removeMask(value: string) {
+    return value.replace(/[^0-9]/g, '')
+  }
+
+  private isValidLength(value: string) {
+    return value.length === 11
+  }
+
+  private allEqualDigits(value: string) {
+    return value.split('').every((digit) => digit === value[0])
+  }
+
+  private mountCheckDigit(value: string) {
+    const calculateCheckDigit1 = this.calculateCheckDigit(value, 10)
+    const calculateCheckDigit2 = this.calculateCheckDigit(value, 11)
+    return `${calculateCheckDigit1}${calculateCheckDigit2}`
+  }
+
+  private calculateCheckDigit(value: string, counter: number) {
+    let total = 0
+    for (const digit of value) {
+      if (counter === 1) break
+      const numericDigit = parseInt(digit)
+      total += numericDigit * counter
+      counter--
+    }
+    const rest = total % 11
+    return rest < 2 ? 0 : 11 - rest
+  }
+
+  private extractCheckDigit(value: string) {
+    return value.slice(9)
+  }
 }
--- test/Cpf.spec.ts
+++ test/Cpf.spec.ts
@@ -1,11 +1,38 @@
 import { Cpf } from '@/domain/entities'
 
 describe('Cpf', () => {
-  it('should create a cpf', () => {
+  it('should create a cpf without mask', () => {
     const input = {
       cpf: '41553189035',
     }
     const output = new Cpf(input.cpf)
-    expect(output.value).toBe('41553189035')
+    expect(output.getCpf()).toBe('41553189035')
+  })
+
+  it('should create a cpf with mask', () => {
+    const input = {
+      cpf: '415.531.890-35',
+    }
+    const output = new Cpf(input.cpf)
+    expect(output.getCpf()).toBe('41553189035')
+  })
+
+  it('should return an error if cpf is empty', () => {
+    expect(() => new Cpf('')).toThrow(new Error('Invalid CPF'))
+  })
+
+  it('should return an error if cpf length is invalid', () => {
+    expect(() => new Cpf('41553')).toThrow(new Error('Invalid CPF'))
+  })
+
+  it.each(['11111111111', '22222222222'])(
+    'should return an error if all digits of the cpf are the same',
+    (cpf: string) => {
+      expect(() => new Cpf(cpf)).toThrow(new Error('Invalid CPF'))
+    },
+  )
+
+  it.each(['12345678910', '10987654321'])('should return an error if cpf is invalid', (cpf: string) => {
+    expect(() => new Cpf(cpf)).toThrow(new Error('Invalid CPF'))
   })
 })
